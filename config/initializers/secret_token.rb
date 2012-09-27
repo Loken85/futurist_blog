923733df1acb0c91e856c6d3a63e3d4238071ddf
Adrian Lindsay <adrianj.lindsay@gmail.com>
2012-09-27 15:14:37 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FuturistBlog::Application.config.secret_token = '4fa83272df21a33071f2d91a77fac1c553af31cf8d199bfd9c3e386284ee5ea561b8287ada8d0952e66ac457360adbbf98501e64a7e33a97994be48a2c0a4b8a'
